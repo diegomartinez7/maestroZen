67e0ade318fb7630d543172fbc21636f0567b644
Diego Martínez <diego777marz@gmail.com>
2022-09-30 02:19:46 -0500

    [added] Componentes de días y Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { Day1Component } from './components/day1/day1.component';
+import { Day2Component } from './components/day2/day2.component';
+import { Day3Component } from './components/day3/day3.component';
+import { Day4Component } from './components/day4/day4.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Day1Component,
+    Day2Component,
+    Day3Component,
+    Day4Component,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
